7780d6753b12c4097e007a7a8130b7c3e0deed86
Lance Johannson <lcheng@campuscruiser.com>
2018-11-19 15:53:33 +0800

    Time line style and its data.

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { LocalDataService } from 'src/app/local-data.service';
+import { TimelineEvent } from '../timeline-event';
 
 @Component({
   selector: 'app-timeline',
@@ -7,14 +9,32 @@ import { Location } from '@angular/common';
   styleUrls: ['./timeline.component.css']
 })
 export class TimelineComponent implements OnInit {
-
-  constructor(private location: Location) { }
+  entries: Array<TimelineEvent>;
+  public alternate: Boolean = false;
+  constructor(private location: Location, private locaDataService: LocalDataService) {
+    locaDataService.getWorkingExp()
+    .subscribe(resp => {
+      this.entries = resp.body;
+    });
+   }
 
   ngOnInit() {
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  onHeaderClick(event) {
+    // do something
+  }
+
+  onDotClick(event) {
+    // do something
+  }
+
+  onExpandEntry(expanded, index) {
+    // console.log(`Expand status of entry #${index} changed to ${expanded}`);
   }
 
 }
