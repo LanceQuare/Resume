cd7a80e5e9496b0ddd4210580b88e417be2b307c
Lance Johannson <lcheng@campuscruiser.com>
2018-12-13 11:41:37 +0800

    Close side menu when scroll.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,6 +56,10 @@ export class AppComponent implements OnInit  {
         } else {
           this.isScreenTop = false;
         }
+
+        if (this.toggle) {
+          this.navToggle();
+        }
       });
     });
   }
